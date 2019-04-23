eaf90296b10e7139c323a8db84241285f85bd8f6
Fabio POMARELLI <fabio.pomarelli@gmail.com>
2019-04-23 16:42:53 +0200

    16h45

--- app/src/main/java/com/ldnr/punissement/ui/main/PageViewModel.java
+++ app/src/main/java/com/ldnr/punissement/ui/main/PageViewModel.java
@@ -12,7 +12,7 @@ public class PageViewModel extends ViewModel {
     private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
-            return "Hello ciao ciao section: " + input;
+            return "Hello git ciao ciao section: " + input;
 
         }
     });
