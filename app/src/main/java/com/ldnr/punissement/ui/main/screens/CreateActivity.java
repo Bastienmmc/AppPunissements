f2367ca48a1a59bd1b7b54cd2d9fd8297d6a5a12
“Florent <f.losada@hotmail.fr>
2019-04-24 14:03:50 +0200

    create activity screen + fragments

---
+++ app/src/main/java/com/ldnr/punissement/ui/main/screens/CreateActivity.java
@@ -0,0 +1,15 @@
+package com.ldnr.punissement.ui.main.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ldnr.punissement.R;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
